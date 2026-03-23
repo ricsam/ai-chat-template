d89d55ee19866c0c53814927cdf5e8866ae56754
Richie <ricsam@users.noreply.github.com>
2026-03-23 23:23:22 +0100

    add passthrough routes

--- shared/router-schema.ts
+++ shared/router-schema.ts
@@ -1,9 +1,14 @@
 import { defineRouterSchema } from "@richie-router/core";
 
-export const routerSchema = defineRouterSchema({
-  __root__: {
-    serverHead: true,
+export const routerSchema = defineRouterSchema(
+  {
+    __root__: {
+      serverHead: true,
+    },
   },
-});
+  {
+    passthrough: ["/api/$"],
+  },
+);
 
 export type RouterSchema = typeof routerSchema;
